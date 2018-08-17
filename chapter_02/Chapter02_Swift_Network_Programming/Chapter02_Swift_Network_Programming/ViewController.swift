51a10239209ecc82536f4e94de6d8b7f99eae508
Larry Johnson <johnson.larry.l@gmail.com>
2018-08-17 18:51:24 -0500

    Cleaned up some crap

---
+++ chapter_02/Chapter02_Swift_Network_Programming/Chapter02_Swift_Network_Programming/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chapter02_Swift_Network_Programming
+//
+//  Created by Larry Johnson on 8/17/18.
+//  Copyright © 2018 Larry Johnson. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
